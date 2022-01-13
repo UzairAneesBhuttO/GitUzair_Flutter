2af077392a667d934262d8bec52768f150eec16d
UzairAneesBhuttO <uzairaneesbhutto@gmail.com>
2022-01-13 15:30:01 +0500

    added files

---
+++ Class_04app/android/app/src/main/kotlin/com/example/class_04app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.class_04app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Class_04app/lib/main.dart
@@ -0,0 +1,164 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatefulWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   _MyAppState createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//             appBar: AppBar(
+//               title: Text("UZAIR APP INTERFACE"),
+//               backgroundColor: Colors.indigoAccent,
+//               actions: [Icon(Icons.dashboard)],
+//               elevation: 30.0,
+//               leading: Icon(Icons.menu),
+//             ),
+//             body: Container(
+//                 child: Padding(
+//               padding: const EdgeInsets.all(8.0),
+//               child: Column(
+//                 children: [
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.blueGrey,
+//                     ),
+//                     title:
+//                         Container(child: Text("Sir Zubair Minhas - ZABTech")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.star,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Zubair Minhas Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.deepPurpleAccent,
+//                     ),
+//                     title: Container(child: Text("Uzair Anees Bhutto")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Uzair Bhutto Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.redAccent,
+//                     ),
+//                     title: Container(child: Text("Vijay Kumar Oad")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Vijay Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.lightBlue,
+//                     ),
+//                     title: Container(child: Text("Tahir Nazeer Babar")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Tahir Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.deepOrangeAccent,
+//                     ),
+//                     title: Container(child: Text("Wasi Ahmed Chandio")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Wasi Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.grey,
+//                     ),
+//                     title: Container(child: Text("Raafiue Rehman Meerani")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Raafiue Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.black87,
+//                     ),
+//                     title: Container(child: Text("Sooraj Hamiraini")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Sooraj Developer")
+//                       ],
+//                     ),
+//                   ),
+//                   ListTile(
+//                     leading: Icon(
+//                       Icons.circle,
+//                       color: Colors.brown,
+//                     ),
+//                     title: Container(child: Text("Sarwan Halepoto")),
+//                     subtitle: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.adb_sharp,
+//                           color: Colors.greenAccent,
+//                         ),
+//                         Text("Hey I'am Sarwan Developer")
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ))));
+//   }
+// }
