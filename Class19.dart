214f9672d86b240afa6199dc3bb3d20d0ee79bea
UzairAneesBhuttO <uzairaneesbhutto@gmail.com>
2022-01-13 15:08:19 +0500

    Hello Uzair

---
+++ Class19.dart
@@ -0,0 +1,35 @@
+// import 'dart:io';
+
+void main() {
+//   stdout.write("ENTER STUDENT ID: ");
+//   int ID = int.parse(stdin.readLineSync()!);
+
+//   switch (ID) {
+//     case 187:
+//       print("");
+//       print("+++++ STUDENT DETAILS +++++");
+//       print("NAME: UZAIR ANEES BHUTTO");
+//       print("REG.NO: 2K18-ITC-187");
+//       print("DEPARTMENT: ITC");
+//       print("SAU TANDOJAM");
+//       break;
+
+//     case 178:
+//       print("");
+//       print("+++++ STUDENT DETAILS +++++");
+//       print("NAME: TAHIR NAZEER BABAR");
+//       print("REG.NO: 2K18-ITC-178");
+//       print("DEPARTMENT: ITC");
+//       print("SAU TANDOJAM");
+//       break;
+
+//     case 136:
+//       print("");
+//       print("+++++ STUDENT DETAILS +++++");
+//       print("NAME: FAHAD ALI");
+//       print("REG.NO: 2K18-BT-136");
+//       print("DEPARTMENT: BIOTECHNOLOGY");
+//       print("SAU TANDOJAM");
+//       break;
+//   }
+}
---
+++ Class21.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+// FILE SYSTEM
+// List VariableName= File("Path");
+
+  // File a = new File('./Uzair.txt');
+  // var b = a.readAsLinesSync();
+  // print(b);
+
+  // File file = File('./Uzair.txt');
+  // print(file.readAsStringSync());
+  // file.writeAsStringSync(file.readAsStringSync() + "HELLOworld");
+
+  // File f = File('./PlayerStats.csv');
+  // print(f.readAsLinesSync());
+}
